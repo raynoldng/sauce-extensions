60d5baca3e39aba30caca794af86f8bf4ef4c805
Jethro Kuan <jethrokuan95@gmail.com>
2017-03-22 20:32:18 +0800

    example usage of plugin

--- router/websocket.js
+++ router/websocket.js
@@ -1,23 +1,25 @@
 import redis from 'redis';
+import SharedbAceRWControl from 'sharedb-ace-rw-control/server';
 import Router from 'koa-router';
 const router = new Router();
 
 const REDIS_URL = "redis://localhost:6379/1";
 
+function pubSubInit(obj, ctx) {
+  const pub = redis.createClient(REDIS_URL);
+  const sub = redis.createClient(REDIS_URL);
+  obj.subscribe(ctx, pub, sub);
+}
+
 router.get('/ws', async (ctx) => {
+  const pub = redis.createClient(REDIS_URL);
+  const sub = redis.createClient(REDIS_URL);
+  
   ctx.websocket.on("error", function(err) {
     console.log(err);
   });
 
-  ctx.websocket.on("message", function(message) {
-    if (message == "server:init") {
-      const sub = redis.createClient(REDIS_URL);
-      sub.on("message", function(channel, message) {
-        ctx.websocket.send(channel+":"+message);
-      });
-      sub.subscribe("access-control");      
-    }
-  });
+  pubSubInit(SharedbAceRWControl, ctx); 
 });
 
 export default router;
