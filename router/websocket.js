053942046c9d97e0adc240a5357b915ebaca390e
Jethro Kuan <jethrokuan95@gmail.com>
2017-03-22 16:24:46 +0800

    remove plugin stuff

--- router/websocket.js
+++ router/websocket.js
@@ -1,5 +1,4 @@
 import redis from 'redis';
-
 import Router from 'koa-router';
 const router = new Router();
 
@@ -9,27 +8,15 @@ router.get('/ws', async (ctx) => {
   ctx.websocket.on("error", function(err) {
     console.log(err);
   });
-  
-  ctx.websocket.on("message", function(message) {
-    let pub;
-    let sub;
-    switch(message) {
-    case "server:init":
-      pub = redis.createClient(REDIS_URL);
-      sub = redis.createClient(REDIS_URL);
 
+  ctx.websocket.on("message", function(message) {
+    if (message == "server:init") {
+      const sub = redis.createClient(REDIS_URL);
       sub.on("message", function(channel, message) {
         ctx.websocket.send(channel+":"+message);
-      }); 
-      sub.subscribe("setReadOnly");
-      break;
-    case "setReadOnly:true":
-      pub = redis.createClient(REDIS_URL);
-      pub.publish("setReadOnly", "true");
-      break;
-    default:
-      console.warn("Unhandled event: " + message);
-    } 
+      });
+      sub.subscribe("access-control");      
+    }
   });
 });
 
