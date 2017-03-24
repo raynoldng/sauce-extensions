53995a5bc17ac900e6e67f8c2185982250d76fd4
Jethro Kuan <jethrokuan95@gmail.com>
2017-03-24 17:22:24 +0800

    update

--- router/websocket.js
+++ router/websocket.js
@@ -1,4 +1,3 @@
-import redis from 'redis';
 import SharedbAceRWControl from 'sharedb-ace-rw-control/server';
 import Router from 'koa-router';
 const router = new Router();
@@ -6,20 +5,16 @@ const router = new Router();
 const REDIS_URL = "redis://localhost:6379/1";
 
 function pubSubInit(obj, ctx) {
-  const pub = redis.createClient(REDIS_URL);
-  const sub = redis.createClient(REDIS_URL);
-  obj.subscribe(ctx, pub, sub);
+  obj.subscribe(ctx);
 }
 
 router.get('/ws', async (ctx) => {
-  const pub = redis.createClient(REDIS_URL);
-  const sub = redis.createClient(REDIS_URL);
-  
   ctx.websocket.on("error", function(err) {
     console.log(err);
   });
 
-  pubSubInit(SharedbAceRWControl, ctx); 
+  const rw = SharedbAceRWControl(REDIS_URL);
+  rw(ctx);
 });
 
 export default router;
